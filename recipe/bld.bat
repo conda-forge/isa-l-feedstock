97f27448f2946a519ad73e995a23f9e9f9ca5f47
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-01-25 09:28:16 +0100

    Do not include igzip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,24 @@
+REM vcpkg install getopt
+REM if errorlevel 1 exit 1
+
+REM set INCLUDE="%INCLUDE%;%CONDA_PREFIX%\Library\share\vcpkg\installed\x86-windows\include
+
 nmake /f Makefile.nmake
 if errorlevel 1 exit 1
 
-nmake igzip.exe /f Makefile.nmake
-if errorlevel 1 exit 1
+REM nmake igzip.exe /f Makefile.nmake
+REM if errorlevel 1 exit 1
 
-copy igzip.exe "%LIBRARY_BIN%\igzip.exe"
-if errorlevel 1 exit 1
+REM copy igzip.exe "%LIBRARY_BIN%\igzip.exe"
+REM if errorlevel 1 exit 1
 
 copy include\isa-l.h %LIBRARY_INCLUDE%\isa-l.h
 if errorlevel 1 exit 1
 
-copy include\isa-l %LIBRARY_INCLUDE%\isa-l
+mkdir %LIBRARY_INCLUDE%\isa-l
+if errorlevel 1 exit 1
+
+copy include\* %LIBRARY_INCLUDE%\isa-l
 if errorlevel 1 exit 1
 
 copy isa-l.dll %LIBRARY_LIB%\isa-l.dll
