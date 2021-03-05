8455d32c01fdcc6d970ac3ff24da0d4278a3079b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-03-05 08:45:36 +0100

    Copy dll to bin directory as it should be

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 xcopy include %LIBRARY_INC%\isa-l\ /E /H /F
 if errorlevel 1 exit 1
 
-copy isa-l.dll %LIBRARY_LIB%\isa-l.dll
+copy isa-l.dll %LIBRARY_BIN%\isa-l.dll
 if errorlevel 1 exit 1
 
 copy isa-l.lib %LIBRARY_LIB%\isa-l.lib
