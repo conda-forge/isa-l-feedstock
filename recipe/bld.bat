f794809c520c218eb92d7c0aafb6bf6b49380f2d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-08-23 09:51:00 +0200

    Build igzip for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,16 @@
 nmake /f Makefile.nmake
 if errorlevel 1 exit 1
 
+nmake/f Makefile.nmake igzip.exe
+if errorlevel 1 exit 1
+
 xcopy include %LIBRARY_INC%\isa-l\ /E /H /F
 if errorlevel 1 exit 1
 
 copy isa-l.dll %LIBRARY_BIN%\isa-l.dll
+if errorlevel 1 exit 1
+
+copy igzip.exe %LIBRARY_BIN%\igzip.exe
 if errorlevel 1 exit 1
 
 copy isa-l.lib %LIBRARY_LIB%\isa-l.lib
