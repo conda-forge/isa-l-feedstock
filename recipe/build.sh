089c56d6d97740022d47811b53ac004530467ca8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-20 11:12:44 +0200

    Remove autotools dependency

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,9 @@
 
 # By patching nasm-filter.sh to include the absolute path to $BUILD_PREFIX/bin/nasm
 # we have guaranteed that the correct nasm is used.
-# Variable AS needs to be set to nasm in order for the configure script to succeed.
-export AS=nasm
 
-./autogen.sh
-./configure --prefix=${PREFIX}
-
-make -j${CPU_COUNT} ${VERBOSE_AT}
-make install
+make -j${CPU_COUNT} -f Makefile.unx ${VERBOSE_AT}
+make -j${CPU_COUNT} -f Makefile.unx prefix=${PREFIX} install
 
 # Remove man pages
 rm -rf ${PREFIX}/share
