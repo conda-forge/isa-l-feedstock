c4815a6c5262f0047caf84deda7eb5bfc71bc5f1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-20 12:06:27 +0200

    Make sure compiler is set

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,8 @@
 # By patching nasm-filter.sh to include the absolute path to $BUILD_PREFIX/bin/nasm
 # we have guaranteed that the correct nasm is used.
 
-make -j${CPU_COUNT} -f Makefile.unx ${VERBOSE_AT}
-make -j${CPU_COUNT} -f Makefile.unx prefix=${PREFIX} install
+make -j${CPU_COUNT} -f Makefile.unx CC=${CC} ${VERBOSE_AT}
+make -j${CPU_COUNT} -f Makefile.unx CC=${CC} prefix=${PREFIX} install
 
 # Remove man pages
 rm -rf ${PREFIX}/share
