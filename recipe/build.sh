cb4756db8419bbb0f760959fabb0ac1da9de862a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-20 15:40:28 +0200

    glibtool workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 # By patching nasm-filter.sh to include the absolute path to $BUILD_PREFIX/bin/nasm
 # we have guaranteed that the correct nasm is used.
 
+# Mac OS assumes glibtool, even though conda environment has libtool.
+ln -s ${BUILD_PREFIX}/bin/libtool ${BUILD_PREFIX}/bin/glibtool
+
 make -d -j${CPU_COUNT} -f Makefile.unx CC=${CC} ${VERBOSE_AT}
 make -d -j${CPU_COUNT} -f Makefile.unx CC=${CC} prefix=${PREFIX} install
 
